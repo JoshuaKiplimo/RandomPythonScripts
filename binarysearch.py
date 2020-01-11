e22c2a9a0335061bd0cdeb8bf9115bd773cd4f0a
JoshuaKiplimo <joshuakiprono8@gmail.com>
2020-01-11 00:45:36 -0500

    with update

--- binarysearch.py
+++ binarysearch.py
@@ -1,26 +1,34 @@
 #A binary search with python. 
 my_list = [num for num in range(0, 101)] # create a list from 0 to 100
 
+def find(listt, num):
+	# it sould be sorted
+	#create upper limit
+	upper = len(listt)-1
+	#create lower limit
+	lower = 0
+	
+	global count
+	count = 0 
+	while lower <= upper: 
+	    middle = int((upper + lower)/2)
+	    count  += 1
 
-# it sould be sorted
-#create upper limit
-upper = max(my_list)
-#create lower limit
-lower = min(my_list)
-target  = int(input('enter input: '))
-count = 0 
-while lower < upper: 
-    middle = int((upper + lower)/2)
-    count  += 1
+	    if target == listt[middle] or target == listt[lower] or target == my_list[upper]:
+	    	return True
+	    	break
+	    #if target is less than the mid value, the upper bound becomes the mid valie
+	    elif target < listt[middle]:
+	         upper = middle
+	         middle = int((lower + upper)/2)
+	    # if target is greater than the mid value, the lower bound becomes the mid value     
+	    else:
+	        lower = middle
+	        middle = int((lower+upper)/2)
 
-    if target == middle or target == lower or target == upper:
-        print('Found! The number '+ str(target) +' was found after ' + str(count-1) + ' half slices')
-        break
-    elif target < middle:
-         upper = middle
-         middle = int((lower + upper)/2)
-         
-    else:
-        lower = middle
-        middle = int((lower+upper)/2)
-        
+target = int(input('enter input: '))	        
+	        
+if find(my_list, target):
+	print('Found! The number '+ str(target) +' was found after ' + str(count-1) + ' half slices')
+else:
+	print('not in the list')
